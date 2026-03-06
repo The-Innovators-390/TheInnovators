5b270aa10b199eae579f51452586a2222e605422
Uroosa786 <145061538+Uroosa786@users.noreply.github.com>
2026-03-06 00:57:44 -0500

    rebase main and applied prettier

--- main/app/_layout.tsx
+++ main/app/_layout.tsx
@@ -7,10 +7,9 @@ import { Stack } from "expo-router";
 import "react-native-reanimated";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-
-import Smartlook from 'react-native-smartlook-analytics';
-import { useEffect, useRef } from 'react';
-import { AppState } from 'react-native';
+import Smartlook from "react-native-smartlook-analytics";
+import { useEffect, useRef } from "react";
+import { AppState } from "react-native";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
@@ -22,19 +21,20 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const appState = useRef(AppState.currentState);
 
-
   useEffect(() => {
-    Smartlook.instance.preferences.setProjectKey('9add208ef165f06a47d19e097a8f5841b9354ac6');
+    Smartlook.instance.preferences.setProjectKey(
+      "9add208ef165f06a47d19e097a8f5841b9354ac6",
+    );
     Smartlook.instance.start();
 
     let stopTimeout: ReturnType<typeof setTimeout> | null = null;
 
-    const subscription = AppState.addEventListener('change', (nextAppState) => {
-      if (nextAppState === 'background') {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (nextAppState === "background") {
         stopTimeout = setTimeout(() => {
           Smartlook.instance.stop();
         }, 1500);
-      } else if (nextAppState === 'active') {
+      } else if (nextAppState === "active") {
         if (stopTimeout) {
           clearTimeout(stopTimeout);
           stopTimeout = null;
