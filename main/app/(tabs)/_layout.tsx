d58e7e8849dba0d59472c22a5697cbb22a0ee138
fouadM200 <124646166+fouadM200@users.noreply.github.com>
2026-03-20 02:50:55 -0400

    [SonarQube]: removing unused imports

--- main/app/(tabs)/_layout.tsx
+++ main/app/(tabs)/_layout.tsx
@@ -1,6 +1,5 @@
-import { Tabs, router } from "expo-router";
+import { Tabs } from "expo-router";
 import React from "react";
-import { Pressable, Text } from "react-native";
 
 import { HapticTab } from "@/components/haptic-tab";
 import { IconSymbol } from "@/components/ui/icon-symbol";
--- main/components/campus/RoutePlanner.tsx
+++ main/components/campus/RoutePlanner.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 type Props = {
